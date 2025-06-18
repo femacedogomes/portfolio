4d0aaeb8e055a0f95513ce408557721e8aa8ba91
Felipe Macedo Gomes <femacedogomes@gmail.com>
2025-06-18 10:38:11 -0300

    fix: next configs

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
+  distDir: "dist",
 };
 
 export default nextConfig;
